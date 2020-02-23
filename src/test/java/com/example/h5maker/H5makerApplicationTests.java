db503ab967ca775d785f182dadbeace1fd0f0754
LiyaCY <2534325832@qq.com>
2020-02-23 16:19:22 +0800

    firstcopmmit

---
+++ src/test/java/com/example/h5maker/H5makerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.h5maker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H5makerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
