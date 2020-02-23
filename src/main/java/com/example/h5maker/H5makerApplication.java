9fb18e6c2ac25cabca3eea91325f909b478a3e26
LiyaCY <2534325832@qq.com>
2020-02-23 16:08:21 +0800

    配置

---
+++ src/main/java/com/example/h5maker/H5makerApplication.java
@@ -0,0 +1,13 @@
+package com.example.h5maker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H5makerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H5makerApplication.class, args);
+	}
+
+}
